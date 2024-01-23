deee81ea2d58560deaba63bc0fb526e22b54af64
Topher Hook <topherhook@gmail.com>
2024-01-23 14:13:17 -0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import logo from '../images/logo.png'
+
+const Header = () => {
+    return (
+        <header >
+            <img src={logo} alt='Topher Hook Logo' />
+        </header>
+    )
+}
+
+export default Header
