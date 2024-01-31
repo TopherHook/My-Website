8898b6b5df09acb365fe4647e2dbed8231bee3a4
Topher Hook <topherhook@gmail.com>
2024-01-30 17:22:01 -0800

    adds Home to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,9 @@ const Navbar = () => {
         <nav className="nav">
             <ul>
                 <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
                     <Link to="/about">About</Link>
                 </li>
                 <li>
