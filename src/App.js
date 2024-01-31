010732b91cd59a974037430ebee66ddb4764ce9c
Topher Hook <topherhook@gmail.com>
2024-01-30 16:37:12 -0800

    adds a Welcome page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
+import Welcome from "./pages/Welcome";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -12,6 +13,7 @@ const App = () => {
             <Navbar />
             <div className="container">
                 <Routes>
+                    <Route path="/" element={<Welcome />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/portfolio" element={<Portfolio />} />
                     <Route path="/contact" element={<Contact />} />
---
+++ src/pages/Welcome.js
@@ -0,0 +1,11 @@
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1 className="pageTitle">Welcome</h1>
+            <p>Hello. My name is Topher Hook, and I built this page using React.</p>
+        </div>
+    )
+}
+
+export default Welcome
